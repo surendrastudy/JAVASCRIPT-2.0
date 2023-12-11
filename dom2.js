0c0697630776df522e0ab0fd503132eb60bd8499
surendrastudy <studymhtcet06@gmail.in>
2023-12-11 16:56:15 +0530

    dom2

--- dom2.js
+++ dom2.js
@@ -1,26 +1,26 @@
-// // let div1 = document.querySelector("div");
-// // console.log(div1);
+//  let div1 = document.querySelector("div");
+// console.log(div1);
 
-// // let id1 = div1.getAttribute("id");
-// // console.log(id1);
+// let id1 = div1.getAttribute("id");
+// console.log(id1);
 
-// // let name1 = div1.getAttribute("name");
-// // console.log(name1);
+//  let name1 = div1.getAttribute("name");
+//  console.log(name1);
 
-// //class 
+//class 
 
-// // let para1 = document.querySelector("p");
-// // console.log(para1.getAttribute("class"));
-// // console.log(para1.setAttribute("class","xyz"))
+// let para1 = document.querySelector("p");
+// console.log(para1.getAttribute("class"));
+// console.log(para1.setAttribute("class","xyz"))
 
 
-// // //style
-// // let div2 = document.querySelector("div"); -----
-// // div2.style.backgroundColor = "green";
+//  style
+//  let div2 = document.querySelector("div"); //-----
+//  div2.style.backgroundColor = "green";
 
-// // div2.style.fontSize = "20px"
-// // div2.innerText = "Hello"  // we can change a text 
-// //  div2.style.visibility = "hidden"
+//  div2.style.fontSize = "20px"
+//  div2.innerText = "Hello"   we can change a text 
+//   div2.style.visibility = "hidden"
 
 // // div2.style.height = "150px";
 
@@ -30,13 +30,13 @@
 //  console.log(newbtn);
 
 //  let div2 = document.querySelector("div"); // we can do for para also
-// // div2.append(newbtn); for in the end of div
-// //  div2.prepend(newbtn); for in the end of div
+//  div2.append(newbtn); for in the end of div
+//   div2.prepend(newbtn); for in the end of div
 
-// // div2.before(newbtn); when we want a button before div
-// //  div2.after(newbtn); when we want a button after div
+//  div2.before(newbtn); when we want a button before div
+//   div2.after(newbtn); when we want a button after div
 
-// // let heading = document.createElement("h1");
+// let heading = document.createElement("h1");
 // heading.innerText = "<i> this is heading is js </i>";
 
 // document.querySelector("body").prepend(heading);
